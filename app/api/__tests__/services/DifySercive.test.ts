1bbafca1ee0d16a72cec5bfcbbb76147c5e189fa
Jun Ito <jujunjun110@gmail.com>
2024-09-21 15:34:48 +0900

    Update: 関数型っぽいテスト

--- app/api/__tests__/services/DifySercive.test.ts
+++ app/api/__tests__/services/DifySercive.test.ts
@@ -1,11 +1,18 @@
 import { DifyService } from "../../services/difyService";
-
 import { container } from "../../utils/container";
+
 test("DifyService", async () => {
   const difyService = container.get<DifyService>(DifyService);
-  const prompt = "東京の首都は？絶対に二文字で答えて";
+  const prompt = "日本の首都は？絶対に2文字で答えて";
   const res = await difyService.generateResponse(prompt);
 
-  expect(res.isOk()).toBeTruthy();
-  expect(res._unsafeUnwrap()).toBe("東京");
+  res.match(
+    (value) => {
+      expect(value).toBe("東京");
+    },
+    (error) => {
+      console.error("Error occurred:", error);
+      throw new Error(`Test failed with error: ${error}`);
+    }
+  );
 });
