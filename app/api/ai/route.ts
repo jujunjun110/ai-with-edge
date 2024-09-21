dcd766a882f60656ed7128f25d36ff745486fa33
Jun Ito <jujunjun110@gmail.com>
2024-09-21 17:48:11 +0900

    Update: 不要なメソッドを追加

--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -1,30 +1,5 @@
-import { NextResponse } from "next/server";
-import { GetResponseUsecase } from "../usecases/getResponseUsecase";
 import { DifyService } from "../services/difyService";
 import { HttpClient } from "../utils/httpClient";
-
-export interface AIResponse {
-  message: string;
-}
-
-export async function GET(request: Request): Promise<NextResponse> {
-  const { searchParams } = new URL(request.url);
-  const prompt = searchParams.get("prompt") || "";
-
-  const httpClient = new HttpClient();
-  const token = process.env.DIFY_TOKEN || "";
-  const difyService = new DifyService(httpClient, token);
-  const usecase = new GetResponseUsecase(difyService);
-  const res = await usecase.execute(prompt);
-
-  if (res.isErr()) {
-    return NextResponse.json(res.error, { status: 500 });
-  }
-
-  const successResult: AIResponse = { message: res.value };
-  return NextResponse.json(successResult);
-}
-
 export const runtime = "nodejs";
 
 export async function POST(request: Request) {
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import Image from "next/image";
 import { useState } from "react";
-// import { AIResponse } from "./api/ai/route";
 
 export default function Home() {
   const [aiResponse, setAiResponse] = useState<string | null>(null);
@@ -11,15 +10,16 @@ export default function Home() {
   const fetchData2 = async (prompt: string) => {
     const res = await fetch(`/api/ai`, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ prompt }),
     });
+
     let done = false;
     let aiResponseText = "";
+
     const reader = res!.body!.getReader();
     const decoder = new TextDecoder("utf-8");
+
     while (!done) {
       const { value, done: readerDone } = await reader.read();
       done = readerDone;
@@ -30,12 +30,6 @@ export default function Home() {
       }
     }
   };
-
-  // const fetchData = async (prompt: string) => {
-  //   const res = await fetch(`/api/ai?prompt=${encodeURIComponent(prompt)}`);
-  //   const data: AIResponse = await res.json();
-  //   setAiResponse(data.message);
-  // };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPrompt(event.target.value);
@@ -48,9 +42,6 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image className="dark:invert" src="https://nextjs.org/icons/next.svg" alt="Next.js logo" width={180} height={38} priority />
-      </main>
       <p>{aiResponse ? <code className="text-[#0070f3]">{aiResponse}</code> : <code className="text-[#0070f3]">Loading...</code>}</p>
       <form onSubmit={handleSubmit}>
         <input type="text" value={prompt} onChange={handleChange} />
