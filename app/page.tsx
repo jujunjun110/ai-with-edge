553ba983c44fdab9c7d9e79976280921b0c1a751
Jun Ito <jujunjun110@gmail.com>
2024-09-21 18:09:00 +0900

    Update: 適当に見栄えを整える

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
-
-import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
   const [aiResponse, setAiResponse] = useState<string | null>(null);
   const [prompt, setPrompt] = useState<string>("");
 
-  const fetchData2 = async (prompt: string) => {
+  const fetchData = async (prompt: string) => {
     const res = await fetch(`/api/ai`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -36,16 +34,18 @@ export default function Home() {
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault(); // デフォルトのフォーム送信を防ぐ
-    fetchData2(prompt);
+    event.preventDefault();
+    fetchData(prompt);
   };
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p>{aiResponse ? <code className="text-[#0070f3]">{aiResponse}</code> : <code className="text-[#0070f3]">Loading...</code>}</p>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={prompt} onChange={handleChange} />
-        <button type="submit">Submit</button>
+      <p>{aiResponse ? <code className="">{aiResponse}</code> : <code className="text-[#0070f3]">Loading...</code>}</p>
+      <form className="fixed bottom-0 flex w-full h-12 bg-red-400  justify-between space-x-5" onSubmit={handleSubmit}>
+        <textarea className="w-4/5 h-full p-4 resize-y border rounded-md text-black" type="text" value={prompt} onChange={handleChange} />
+        <button className="w-1/5 h-full bg-slate-800 p-4" type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
